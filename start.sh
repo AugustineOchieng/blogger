42542fe20b6b42b6519baa23d1e6169ff2bf0a76
AugustineOchieng <gusochieng@gmail.com>
2019-04-28 22:11:29 +0300

    Crete s

---
+++ start.sh
@@ -0,0 +1,8 @@
+export SECRET_KEY="augustine"
+
+export MAIL_USERNAME="gustin9tis@gmail.com"
+export MAIL_PASSWORD="gus9999#"
+
+export DATABASE_URL='postgresql+psycopg2://moringa:augustine@localhost/blogger'
+
+python3.6 manage.py server
