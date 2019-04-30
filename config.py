98c7e9b1468be0ba9b96c69a4c46ee23e74bf2a9
AugustineOchieng <gusochieng@gmail.com>
2019-04-30 11:44:54 +0300

    Add URI to Prodconfig class

--- config.py
+++ config.py
@@ -1,81 +1,52 @@
-# import os
+import os
 
 
-# class Config:
-
-#     SECRET_KEY = os.environ.get("SECRET_KEY")
-#     # SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringa:augustine@localhost/pitch"
-
-#     UPLOADED_PHOTOS_DEST = "app/static/photos"
-
-#     #  email configurations
-#     MAIL_SERVER = "smtp.googlemail.com"
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
-#     @staticmethod
-#     def init_app(app):
-#         pass
-
-
-# class ProdConfig(Config):
-#     """
-#     Production  configuration child class
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     """
-
-#     pass
-#     # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
-
-# class TestConfig(Config):
-#     pass
-
-
-# class DevConfig(Config):
-#     """
-#     Development  configuration child class
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     """
-
-#     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringa:augustine@localhost/pitch"
-
-#     DEBUG = True
-
-
-# config_options = {
-#     "development": DevConfig,
-#     "production": ProdConfig,
-#     "test": TestConfig,
-# }
 class Config:
-    '''
-    General configuration parent class
-    '''
-    pass
 
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringa:augustine@localhost/blogger"
+
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
+
+    #  email configurations
+    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 
 class ProdConfig(Config):
-    '''
+    """
     Production  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
-    '''
+    """
+
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class TestConfig(Config):
     pass
 
 
 class DevConfig(Config):
-    '''
+    """
     Development  configuration child class
-
     Args:
         Config: The parent configuration class with General configuration settings
-    '''
+    """
+
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringa:augustine@localhost/blogger"
 
     DEBUG = True
+
+
+config_options = {
+    "development": DevConfig,
+    "production": ProdConfig,
+    "test": TestConfig,
+}
